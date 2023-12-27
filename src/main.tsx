dfdac86fe5feff92f5faa26d1decae0d169b7823
burakbasher <burak.baser_@hotmail.com>
2023-12-27 16:35:17 +0300

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.css'
 import { router } from "./routes/Routes"
 import { RouterProvider } from "react-router-dom"
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-)
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
