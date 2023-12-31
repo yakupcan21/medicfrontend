342ba4931df7ca90ce137fcdfc482bcd9dc03e3b
burakbasher <burak.baser_@hotmail.com>
2023-12-31 05:38:39 +0300

    updated Profile Page

--- src/pages/profilepage/ProfilePage.tsx
+++ src/pages/profilepage/ProfilePage.tsx
@@ -1,22 +1,56 @@
-import React from 'react';
-//import Navbar from '../components/menu/NavbarComponent';
+import React, { useEffect, useRef, useState } from 'react';
 import './ProfilePage.scss'; // Make sure to adjust the path accordingly
 import Navbar from '../../components/menu/NavbarComponent';
 import Profile from '../../components/ProfileComponent';
 import BackgroundMotion from '../../components/BackgroundMotion';
 import { Reveal } from '../../components/Reveal';
+import penImg from '../../assets/penImg.png';
+import { color } from 'framer-motion';
 
 interface ProfilePageProps { }
 
 const ProfilePage: React.FC<ProfilePageProps> = () => {
-  const kg = 95;
-  const boy = 190;
-  const vki = (95 / ((190 / 100) ** 2)).toFixed(3);
+  const [eMailInput, seteMailInput] = useState<string>('');
+  const [phoneNumInput, setPhoneNumInput] = useState<string>('');
+  const [warningPhone, setWarningPhone] = useState(false);
+  const [warningeMail, setWarningeMail] = useState(false);
+
+  const profileName = "Buğra Burak Başer";
+  const profileSNum = "28*******85";
+  const profileDateOfBirth = "29/12/2000";
+  const [profileEMail, setProfileEMail] = useState<string>("bugraburakbaser@gmail.com");
+  const [profilePhoneNum, setProfilePhoneNum] = useState<string>("+90 537 271 35 91");
+
+  const updateeMail = () => {
+    const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(eMailInput != '' && emailRegex.test(eMailInput)){
+      setProfileEMail(eMailInput);
+      setWarningeMail(false);
+    } else if (eMailInput != '' && !emailRegex.test(phoneNumInput)) {
+      setWarningeMail(true);
+    }
+    seteMailInput('');
+};
+
+const updatePhoneNum = () => {
+  const phoneNumRegex = /^\+\d{2}\s?\d{3}\s?\d{3}\s?\d{2}\s?\d{2}$/;
+
+  if(phoneNumInput != '' && phoneNumRegex.test(phoneNumInput)){
+    setProfilePhoneNum(phoneNumInput);
+    setWarningPhone(false);
+  } else if (phoneNumInput != '' && !phoneNumRegex.test(phoneNumInput)) {
+    setWarningPhone(true);
+  }
+  
+  setPhoneNumInput('');
+};
+
 
   return (
     <>
-      <Navbar isDoctor={false} isPatient={true} />
       <BackgroundMotion />
+      <Navbar isDoctor={false} isPatient={true} />
       <div className="container">
         <div className="left-panel">
           <div className="profile-component-profile-page-position">
@@ -25,66 +59,51 @@ const ProfilePage: React.FC<ProfilePageProps> = () => {
         </div>
         <div className="right-panel">
           <div className="content-container">
-            <div className="upper">
-              <h1>Genel Bilgiler</h1>
-              <div>
+            <div className="upper" >
+              <div className='informations-header'>Genel Bilgiler</div>
+
                 <div>
-                  <div>
-                    <h5>İsim Soyisim</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">İlyas Yakup Pekgüç</div>
+                  <div className='information-type'>İsim Soyisim</div>
+                  <input type="text" className="information-input" id='disabled' placeholder={profileName} disabled={true} />
+                  <hr />
                 </div>
-                <hr />
                 <div>
-                  <div>
-                    <h5>Email</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">lol@gmail.com</div>
+                  <div className='information-type'>TC Kimlik No</div>
+                  <input type="text" className="information-input" id='disabled' placeholder={profileSNum} disabled={true} />
+                  <hr />
                 </div>
-                <hr />
                 <div>
-                  <div>
-                    <h5>Telefon</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">05457382023</div>
+                  <div className='information-type'>Doğum Tarihi</div>
+                  <input type="text" className="information-input" id='disabled' placeholder={profileDateOfBirth} disabled={true} />
+                  <hr />
                 </div>
-                <hr />
                 <div>
-                  <div>
-                    <h5>Adress</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">Ankara</div>
+                  <div className='information-type'>Telefon Numarası</div>
+                  <input type="text" className="information-input" placeholder={profilePhoneNum} value={phoneNumInput} onChange={(e) => setPhoneNumInput(e.target.value)} />
+                  <img src={penImg} className="pen-icon" onClick={updatePhoneNum}/>
+                  {!warningPhone && <hr />}
+                  {warningPhone && <hr style={{ border: '1px solid #DC5353', boxShadow: '0px 3px 6px #DC5353'}} />}
                 </div>
-                <hr />
                 <div>
-                  <div>
-                    <h5>TC Kimlik No</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">26*******71</div>
+                  <div className='information-type'>E-posta Adresi</div>
+                  <input type="text" className="information-input" placeholder={profileEMail} value={eMailInput} onChange={(e) => seteMailInput(e.target.value)} />
+                  <img src={penImg} className="pen-icon" onClick={updateeMail}/>
+                  {!warningeMail && <hr />}
+                  {warningeMail && <hr style={{ border: '1px solid #DC5353', boxShadow: '0px 3px 6px #DC5353'}} />}
                 </div>
-                <hr />
                 <div>
-                  <div>
-                    <h5>TC Kimlik No</h5>
-                  </div>
-                  <div className="genelBilgilerInfo">26*******71</div>
+                  <div className='information-type'>Parola</div>
+                  <input type="text" className="information-input" placeholder="*************" />
+                  <img src={penImg} className="pen-icon"/>
+                  {!warningeMail && <hr />}
+                  {warningeMail && <hr style={{ border: '1px solid #DC5353', boxShadow: '0px 3px 6px #DC5353'}} />}
                 </div>
-                <hr />
-              </div>
             </div>
             <Reveal>
               <div className="bottom">
-                <h1>Recent hastalıklar</h1>
-                <div>
-                  <div>
-                    <div>
-                      <h5>Hastalık çeşidi</h5>
-                    </div>
-                    <div className="genelBilgilerInfo">
-                      hastalık açıklaması
-                    </div>
-                  </div>
-                </div>
+                <div className='informations-header'>Geçmiş Hastalıklar</div>
+                <div className='information-type'>Hastalık Çeşidi</div>
+                <div className='information-statement'>Hastalık açıklaması</div>
               </div>
             </Reveal>
           </div>
