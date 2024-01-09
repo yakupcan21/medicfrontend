47e7514385f4be5569b85e9fd731f7ea95408b6f
Zeyn28 <zeynepaygun06@gmail.com>
2024-01-09 23:55:48 +0300

    wip

--- src/main.tsx
+++ src/main.tsx
@@ -8,12 +8,13 @@ import { RouterProvider } from "react-router-dom"
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
-);
+)
 
 
-root.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
+
 
